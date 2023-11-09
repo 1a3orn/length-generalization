212c1b14a449a1efe3eee322baac9dce129d9cea
1a3orn <1a3orn@protonmail.com>
2023-11-09 15:13:09 -0500

    CASD

--- args.py
+++ args.py
@@ -62,7 +62,7 @@ parser.add_argument('--train_max_steps', type=int, default=25000)
 parser.add_argument('--train_batch_size', type=int, default=64)
 parser.add_argument("--train_acc_stop", type=float, default=0.999)
 parser.add_argument("--train_log_every", type=int, default=250)
-parser.add_argument("--train_lr", type=float, default=0.001)
+parser.add_argument("--train_lr", type=float, default=0.0005)
 
 def get_args():
     result = parser.parse_args()
