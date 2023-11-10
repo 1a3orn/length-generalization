4b7afb77e5892b08321bce623617528d64cf7700
1a3orn <1a3orn@protonmail.com>
2023-11-10 15:36:57 -0500

    ASD

--- models/karpathy_trans.py
+++ models/karpathy_trans.py
@@ -146,14 +146,12 @@ class GPT(nn.Module):
             ln_f = LayerNorm(config.n_embd, bias=config.bias),
         ))
         self.proj_up = nn.Linear(config.n_embd // 16, config.n_embd)
-        torch.nn.init.uniform_(self.transformer.wte.weight, -0.0001, 0.0001)
-        torch.nn.init.uniform_(self.transformer.wpe.weight, -0.0001, 0.0001)
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
         # with weight tying when using torch.compile() some warnings get generated:
         # "UserWarning: functional_call was passed multiple values for tied weights.
         # This behavior is deprecated and will be an error in future versions"
         # not 100% sure what this is, so far seems to be harmless. TODO investigate
-        self.transformer.wte.weight = self.lm_head.weight # https://paperswithcode.com/method/weight-tying
+        #self.transformer.wte.weight = self.lm_head.weight # https://paperswithcode.com/method/weight-tying
 
         # init all weights
         self.apply(self._init_weights)
