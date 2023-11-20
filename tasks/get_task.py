8b8c1734daacc506b38498d0f116eba30f18a75d
1a3orn <1a3orn@protonmail.com>
2023-11-20 14:57:07 -0500

    ASD

--- tasks/get_task.py
+++ tasks/get_task.py
@@ -25,6 +25,7 @@ from tasks.unary_if_classes import UnaryIf
 
 
 def get_task(task_name: str) -> object:
+    task_name = task_name.trim()
     if task_name == "count":
         return Count()
     elif task_name == "mode":
